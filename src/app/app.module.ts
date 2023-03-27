03b736034785921e3d78a1b0f1f6b21c2fd4b3c5
antoine.BERTHILLOT <antoine.berthillot@gcatrans.com>
2023-03-27 14:33:31 +0200

    etape 2 display list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
