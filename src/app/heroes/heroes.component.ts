e63791831e01be9b5c690fad9f7a1620921fd094
antoine.BERTHILLOT <antoine.berthillot@gcatrans.com>
2023-03-27 15:30:03 +0200

    observable data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -12,11 +12,21 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm',
   };
-
-  heroes = HEROES;
-
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
